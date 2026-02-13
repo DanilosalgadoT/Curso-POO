1141f38a2d5e8fceef56abee2af9ba2006cc1f1c
DanilosalgadoT <dsalgadot@unal.edu.co>
2026-02-12 21:30:46 -0500

    Add files via upload

---
+++ Ejercicio 14/Calculos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio14;
+
+/**
+ *
+ * @author danil
+ */
+public class Calculos {
+    public static double calcular_cuadrado(double numero) {
+        return Math.pow(numero,2);
+    }
+    public static double calcular_cubo(double numero){
+        return Math.pow(numero,3);
+    }
+}
