29eb7dd5d59ee66a71a0c262817561255175acd8
DanilosalgadoT <dsalgadot@unal.edu.co>
2026-05-28 19:16:41 -0500

    Add files via upload

---
+++ Actividad 3/Ejercicio 8.2/Ejercicio8p2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio8p2;
+
+/**
+ *
+ * @author danil
+ */
+public class Ejercicio8p2 {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); }
+
+    }
+
