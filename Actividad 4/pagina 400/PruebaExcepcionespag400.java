3f9ae7ec7f5ca298cdb8d8ec9a021498c009d727
DanilosalgadoT <dsalgadot@unal.edu.co>
2026-06-19 14:11:42 -0500

    Add files via upload

---
+++ Actividad 4/pagina 400/PruebaExcepcionespag400.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaexcepcionespag400;
+
+
+public class PruebaExcepcionespag400 {
+    
+    public static void main(String[] args){
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);       
+    }
+}
